50bbf350040b90490fe8500840c41c1a0329f373
g00nd0 <mitchellgoon@hotmail.com>
2021-04-14 16:34:49 +0800

    Clean up unused or commented code

--- client-react/src/pages/account/AccountEdit.js
+++ client-react/src/pages/account/AccountEdit.js
@@ -3,30 +3,26 @@ import { useParams, Redirect } from "react-router-dom";
 const jwt = require("jsonwebtoken");
 
 const AccountEdit = () => {
-    // const userId = props.user.userId
-    // console.log("userId from props.userId at account edit", userId)
-    const userIdParam = useParams().id;
+  // const userId = props.user.userId
+  const userIdParam = useParams().id;
+  const token = localStorage.getItem("token");
+  const decoded = jwt.verify(token, "sei-26"); //cant read secret :/
+  const user = { userId: decoded.user._id, username: decoded.user.username };
+  return (
+    <>
+      {user.userId === userIdParam ? (
+        <div className="form-box">
+          <div class="form-h1">
+            <h1>Edit account</h1>
+          </div>
 
-    const token = localStorage.getItem("token");
-    const decoded = jwt.verify(token, "sei-26");//cant read secret :/
-    const user = { userId: decoded.user._id, username: decoded.user.username }
-    console.log("user", user)
-    return (
-        <>
-            {user.userId === userIdParam ? (
-                <div className="form-box">
-
-                    <div class="form-h1">
-                        <h1>Edit account</h1>
-                    </div>
-
-                    <AccountDetailsForm user={user} />
-                </div>
-            ) : (
-                    <Redirect to={"/restricted"} />
-                )}
-        </>
-    );
+          <AccountDetailsForm user={user} />
+        </div>
+      ) : (
+        <Redirect to={"/restricted"} />
+      )}
+    </>
+  );
 };
 
 export default AccountEdit;
